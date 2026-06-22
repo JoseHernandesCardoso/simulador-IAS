08219542d1855f4646134109e5d87b2ff42e0270
JoseHernandesCardoso <josehhernandescardoso@gmail.com>
2026-06-22 15:44:56 -0300

    Aprimoramento da leitura de instruções. Aceita linhas vazias e comentários

--- io/read_file.c
+++ io/read_file.c
@@ -8,32 +8,55 @@
 
 /**
  * Lê a próxima linha do arquivo de instruções.
+ * Texto precedido por "#" é ignorado.
+ * Retorna 1 se a linha for copiada com sucesso e possui conteúdo.
+ * Caso contrário, retorna 0.
  */
-static void read_next_line(char *output, FILE *file) {
-    fgets(output, MAX_LINE_SIZE, file);
-    output[strcspn(output, "\n")] = '\0';
+static int read_next_line(char *output, FILE *file) {
+    int success;
+    if (fgets(output, MAX_LINE_SIZE, file) != NULL) {
+        output[strcspn(output, "\n")] = '\0';
+        output[strcspn(output, "#")] = '\0';
+        success = 1;
+    }
+
+    if (success && output[0] != '\0') return 1;
+    else return 0;
 }
 
 /**
  * Lê o arquivo de instruções e salva elas na memória.
+ * Conteúdo precedido por "#" é considerado comentário e é ignorado.
  */
 void read_code_file(char *file_name, Memory memory) {
     FILE *file;
     char line[MAX_LINE_SIZE];
     BinaryWord inst1, inst2;
-    int write_address = INSTRUCTIONS_ADDRESS;
+    int valid_line, write_address = INSTRUCTIONS_ADDRESS;
 
     file = fopen(file_name, "r");
     while (!feof(file) && write_address < MAX_MEMORY_SIZE) {
-        read_next_line(line, file);
-        inst1 = encode_instruction(line);
+        valid_line = 0;
+        while (!feof(file) && !valid_line) {
+            if (read_next_line(line, file)) {
+                valid_line = 1;
+            }
+        };
 
-        if (!feof(file)) {
-            read_next_line(line, file);
-            inst2 = encode_instruction(line);
-        } else {
-            inst2 = EMPTY_WORD;
+        if (valid_line) {
+            inst1 = encode_instruction(line);
         }
+
+        valid_line = 0;
+        while (!feof(file) && !valid_line) {
+            if (read_next_line(line, file)) {
+                valid_line = 1;
+            }
+        };
+
+        if (valid_line) {
+            inst2 = encode_instruction(line);
+        }       
 
         memory[write_address] = (inst1 << INSTRUCTION_SIZE) | inst2;
         write_address++;
