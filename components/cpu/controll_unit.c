9edcd53757f06f86da6d2a4e98b32730a1f37dd6
JoseHernandesCardoso <josehhernandescardoso@gmail.com>
2026-06-27 16:28:08 -0300

    Movimentações de dados agora mostram o valor que está sendo transferido

--- components/cpu/controll_unit.c
+++ components/cpu/controll_unit.c
@@ -5,15 +5,20 @@
 int next_instruction_IBR = 0;
 
 void fetch_cycle(RegistrarsBank reg_bank, Memory mem) {
+    BinaryWord buffer;
     // Próxima instrução no IBR?
     if (next_instruction_IBR) {
-        printf(" IR <- IBR(0:7)\n");
-        reg_bank->IR = slice(reg_bank->IBR, 0, 7, 20);
-        printf("MAR <- IBR(8:19)\n");
-        reg_bank->MAR = slice(reg_bank->IBR, 8, 19, 20);
+        buffer = slice(reg_bank->IBR, 0, 7, 20);
+        printf(" IR <- IBR(0:7) [%lld]\n", buffer);
+        reg_bank->IR = buffer;
+
+        buffer = slice(reg_bank->IBR, 8, 19, 20);
+        printf("MAR <- IBR(8:19) [%lld]\n", buffer);
+        reg_bank->MAR = buffer;
         next_instruction_IBR = 0;
-        printf(" PC <- PC + 1\n");
+
         reg_bank->PC++;
+        printf(" PC <- PC + 1 [%lld]\n", reg_bank->PC);
 
     } else {
         // Fim da memória
@@ -21,7 +26,7 @@ void fetch_cycle(RegistrarsBank reg_bank, Memory mem) {
             reg_bank->IR = EMPTY_WORD;
         
         } else {
-            printf("MAR <- PC\n");
+            printf("MAR <- PC [%lld]\n", reg_bank->PC);
             reg_bank->MAR = reg_bank->PC;
             printf("MBR <- M(MAR)\n");
             reg_bank->MBR = mem[reg_bank->MAR];
@@ -31,140 +36,169 @@ void fetch_cycle(RegistrarsBank reg_bank, Memory mem) {
                 printf("IBR <- MBR(20:39)\n");
                 reg_bank->IBR = slice(reg_bank->MBR, 20, 39, 40);
                 next_instruction_IBR = 1;
-                printf(" IR <- MBR(0:7)\n");
-                reg_bank->IR = slice(reg_bank->MBR, 0, 7, 40);
-                printf("MAR <- MBR(8:19)\n");
-                reg_bank->MAR = slice(reg_bank->MBR, 8, 19, 40);
+
+                buffer = slice(reg_bank->MBR, 0, 7, 40);
+                printf(" IR <- MBR(0:7) [%lld]\n", buffer);
+                reg_bank->IR = buffer;
+
+                buffer = slice(reg_bank->MBR, 8, 19, 40);
+                printf("MAR <- MBR(8:19) [%lld]\n", buffer);
+                reg_bank->MAR = buffer;
 
             } else {
-                printf(" IR <- MBR(20:27)\n");
-                reg_bank->IR = slice(reg_bank->MBR, 20, 27, 40);
-                printf("MAR <- MBR(28:39)\n");
-                reg_bank->MAR = slice(reg_bank->MBR, 28, 39, 40);
-                printf(" PC <- PC + 1\n");
+                buffer = slice(reg_bank->MBR, 20, 27, 40);
+                printf(" IR <- MBR(20:27 [%lld])\n", buffer);
+                reg_bank->IR = buffer;
+
+                buffer = slice(reg_bank->MBR, 28, 39, 40);
+                printf("MAR <- MBR(28:39) [%lld]\n", buffer);
+                reg_bank->MAR = buffer;
+
                 reg_bank->PC++;
+                printf(" PC <- PC + 1 [%lld]\n", reg_bank->PC);
             }
         }
     }
 }
 
 void execute_cycle(RegistrarsBank reg_bank, Memory mem) {
+    BinaryWord buffer;
+
     switch (reg_bank->IR) {
         case 1: // LOAD M(X)
             printf("LOAD M(%lld)\n", reg_bank->MAR);
-            printf("AC <- M[%lld]\n", reg_bank->MAR);
+
             reg_bank->AC = mem[reg_bank->MAR];
+            printf("AC <- M(%lld) [%lld]\n", reg_bank->MAR, reg_bank->AC);
             break;
 
         case 2: // LOAD -M(X)
             printf("LOAD -M(%lld)\n", reg_bank->MAR);
-            printf("AC <- -M[%lld]\n", reg_bank->MAR);
+
             reg_bank->AC = -mem[reg_bank->MAR];
+            printf("AC <- -M(%lld) [%lld]\n", reg_bank->MAR, reg_bank->AC);
             break;
 
         case 3: // LOAD |M(X)|
             printf("LOAD |M(%lld)|\n", reg_bank->MAR);
-            printf("AC <- |M[%lld]|\n", reg_bank->MAR);
+
             reg_bank->AC = abs(mem[reg_bank->MAR]);
+            printf("AC <- |M(%lld)| [%lld]\n", reg_bank->MAR, reg_bank->AC);
             break;
 
         case 4: // LOAD -|M(X)|
             printf("LOAD -|M(%lld)|\n", reg_bank->MAR);
-            printf("AC <- -|M[%lld]|\n", reg_bank->MAR);
+
             reg_bank->AC = -abs(mem[reg_bank->MAR]);
+            printf("AC <- -|M(%lld)| [%lld]\n", reg_bank->MAR, reg_bank->AC);
             break;
 
         case 9: // LOAD MQ,M(X)
             printf("LOAD MQ,M(%lld)\n", reg_bank->MAR);
-            printf("MQ <- M[%lld]\n", reg_bank->MAR);
+
             reg_bank->MQ = mem[reg_bank->MAR];
+            printf("MQ <- M(%lld) [%lld]\n", reg_bank->MAR, reg_bank->MQ);
             break;
 
         case 10: // LOAD MQ
             printf("LOAD MQ\n");
-            printf("AC <- MQ\n");
+
             reg_bank->AC = reg_bank->MQ;
+            printf("AC <- MQ [%lld]\n", reg_bank->AC);
             break;
 
         case 18: // STOR M(X,8:19)
             printf("STOR M(%lld,8:19)\n", reg_bank->MAR);
-            printf("M[%lld](8:19) <- PC(27:39)\n", reg_bank->MAR);
+
+            buffer = slice(reg_bank->AC, 27, 39, 40);
             mem[reg_bank->MAR] &= ~(INST_ADDR_MASK << 20);
-            mem[reg_bank->MAR] |= slice(reg_bank->AC, 27, 39, 40) << 20;
+            mem[reg_bank->MAR] |= buffer << 20;
+            printf("M(%lld)(8:19) <- PC(27:39) [%lld]\n", reg_bank->MAR, buffer);
             break;
 
         case 19: // STOR M(X,28:39)
             printf("STOR M(%lld,28:39)\n", reg_bank->MAR);
-            printf("M[%lld](28:39) <- PC(27:39)\n", reg_bank->MAR);
+
+            buffer = slice(reg_bank->AC, 27, 39, 40);
             mem[reg_bank->MAR] &= ~INST_ADDR_MASK;
-            mem[reg_bank->MAR] |= slice(reg_bank->AC, 27, 39, 40);
+            mem[reg_bank->MAR] |= buffer;
+            printf("M(%lld)(28:39) <- PC(27:39) [%lld]\n", reg_bank->MAR, buffer);
             break;
 
         case 33: // STOR M(X)
             printf("STOR M(%lld)\n", reg_bank->MAR);
-            printf("M[%lld] <- AC\n", reg_bank->MAR);
+
             mem[reg_bank->MAR] = reg_bank->AC;
+            printf("M(%lld) <- AC [%lld]\n", reg_bank->MAR, reg_bank->AC);
             break;
 
         case 13: // JUMP M(X,0:19)
             printf("JUMP M(%lld,0:19)\n", reg_bank->MAR);
-            printf("PC <- MAR\n");
+
             reg_bank->PC = reg_bank->MAR;
             next_instruction_IBR = 0;
+            printf("PC <- MAR, [%lld]\n", reg_bank->PC);
             break;
 
         case 14: // JUMP M(X,20:39)
             printf("JUMP M(%lld,20:39)\n", reg_bank->MAR);
-            printf("PC <- MAR\n");
+
             reg_bank->PC = reg_bank->MAR;
             next_instruction_IBR = 0;
+            printf("PC <- MAR [%lld]\n", reg_bank->PC);
             break;
     
         case 15: // JUMP+ M(X,0:19)
             printf("JUMP+ M(%lld,0:19)\n", reg_bank->MAR);
+
             if (reg_bank->AC >= 0) {
-                printf("PC <- MAR\n");
                 reg_bank->PC = reg_bank->MAR;
                 next_instruction_IBR = 0;
+                printf("PC <- MAR [%lld]\n", reg_bank->PC);
             }
             break;
 
         case 16: // JUMP+ M(X,20:39)
             printf("JUMP+ M(%lld,20:39)\n", reg_bank->MAR);
+
             if (reg_bank->AC >= 0) {
-                printf("PC <- MAR\n");
                 reg_bank->PC = reg_bank->MAR;
                 next_instruction_IBR = 0;
+                printf("PC <- MAR [%lld]\n", reg_bank->PC);
             }
             break;
         
         case 5: // ADD M(X)
             printf("ADD M(%lld)\n", reg_bank->MAR);
-            printf("AC <- AC + M[%lld]\n", reg_bank->MAR);
+
             reg_bank->AC += mem[reg_bank->MAR];
+            printf("AC <- AC + M(%lld) [%lld]\n", reg_bank->MAR, reg_bank->AC);
             break;
         
         case 7: // ADD |M(X)|
             printf("|ADD M(%lld)|\n", reg_bank->MAR);
-            printf("AC <- AC + |M[%lld]|\n", reg_bank->MAR);
+
             reg_bank->AC += abs(mem[reg_bank->MAR]);
+            printf("AC <- AC + |M(%lld)| [%lld]\n", reg_bank->MAR, reg_bank->AC);
             break;
 
         case 6: // SUB M(X)
             printf("SUB M(%lld)\n", reg_bank->MAR);
-            printf("AC <- AC - M[%lld]\n", reg_bank->MAR);
+
             reg_bank->AC -= mem[reg_bank->MAR];
+            printf("AC <- AC - M(%lld) [%lld]\n", reg_bank->MAR, reg_bank->AC);
             break;
 
         case 8: // SUB |M(X)|
             printf("SUB |M(%lld)|\n", reg_bank->MAR);
-            printf("AC <- AC - |M[%lld]|\n", reg_bank->MAR);
+
             reg_bank->AC -= abs(mem[reg_bank->MAR]);
+            printf("AC <- AC - |M(%lld)| [%lld]\n", reg_bank->MAR, reg_bank->AC);
             break;
         
         case 11: // MUL M(X)
             printf("MUL M(%lld)\n", reg_bank->MAR);
-            printf("AC,MQ <- MQ * mem[%lld]\n", reg_bank->MAR);
+
             // Algoritmo da multiplicação binária
             reg_bank->AC = 0ULL;
             for (int i=0; i < 40; i++) {
@@ -176,26 +210,30 @@ void execute_cycle(RegistrarsBank reg_bank, Memory mem) {
                 reg_bank->MQ |= (reg_bank->AC & 1ULL) << 39;
                 reg_bank->AC >>= 1;
             }
+            printf("AC,MQ <- MQ * M(%lld) [%lld | %lld]\n", reg_bank->MAR, reg_bank->AC, reg_bank->MQ);
             break;
 
         case 12: // DIV M(X)
             printf("DIV M(%lld)\n", reg_bank->MAR);
-            printf("MQ <- AC / M[%lld]\n", reg_bank->MAR);
-            printf("AC <- AC %% M[%lld]\n", reg_bank->MAR);
+
             reg_bank->MQ = reg_bank->AC / mem[reg_bank->MAR];
             reg_bank->AC = reg_bank->AC % mem[reg_bank->MAR];
+            printf("MQ <- AC / M(%lld) [%lld]\n", reg_bank->MAR, reg_bank->MQ);
+            printf("AC <- AC %% M(%lld) [%lld]\n", reg_bank->MAR, reg_bank->AC);
             break;
 
         case 20: // LSH
             printf("LSH\n");
-            printf("AC <- AC <<\n");
+
             reg_bank->AC <<= 1;
+            printf("AC <- AC << [%lld]\n", reg_bank->AC);
             break;
 
         case 21: // RSH
             printf("RSH\n");
-            printf("AC <- AC >>\n");
+
             reg_bank->AC >>= 1;
+            printf("AC <- AC >> [%lld]\n", reg_bank->AC);
             break;
 
         default:
